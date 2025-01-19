8e4ce42073b73dc3c6f6021272dc307622dae4f9
Adarsh Kumar Nishad <69967272+adars777@users.noreply.github.com>
2025-01-19 16:03:14 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://real-chat-app-w83z.onrender.com/api/",
+  baseURL: "http://real-chat-app-w83z.onrender.com/api/",
   withCredentials: true,
 });
