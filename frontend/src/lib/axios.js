6e4aae50a0708729d619e636429e71c8d2041530
Adarsh Kumar Nishad <69967272+adars777@users.noreply.github.com>
2025-09-24 15:21:40 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-app-backend-qpc8.onrender.com/api/",
+  baseURL: "https://chat-app-backend-qpc8.onrender.com/api",
   withCredentials: true,
 });
