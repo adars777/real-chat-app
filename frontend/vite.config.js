8eab5b45cd10cffc67487c6391bfec39fa66ec0c
Adarsh Kumar Nishad <69967272+adars777@users.noreply.github.com>
2025-11-13 23:07:50 +0530

    Configure proxy for API in Vite server
    Added proxy configuration for API requests.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
+ server: {
+    proxy: {
+      "/api": "https://chat-app-backend-qpc8.onrender.com",
+    },
   },
 })
