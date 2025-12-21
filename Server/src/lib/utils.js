2ef445fea1578a2202bb550d139a8fddc46a6450
Adarsh Kumar Nishad <69967272+adars777@users.noreply.github.com>
2025-12-21 14:29:18 +0530

    Update utils.js

--- Server/src/lib/utils.js
+++ Server/src/lib/utils.js
@@ -10,7 +10,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, //in mili second
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
    // sameSite: "Strict", // CSRF attacts cross-site request forgery attacks
-   sameSite": none,
+   sameSite: "none",
     // secure: process.env.NODE_ENV !== "Development",
    secure:true,
   });
